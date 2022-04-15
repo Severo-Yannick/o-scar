72be493b78ce5a3c15bb5944430edd3cef31ce61
Severo-Yannick <severoyannick@gmail.com>
2022-04-15 09:59:09 +0200

    pool PG

---
+++ app/db/pg.js
@@ -0,0 +1,35 @@
+/**
+ * Plutôt que de créer et connecté un Client
+ * On va plutôt créer un "pool" de client et
+ * laisser notre module manager les connexions
+ * de plusieurs client en fonction des besoins.
+ *
+ * l'objet de pool à aussi une méthode query donc le reste du code
+ * continuera de fonctionner
+ *
+ * Comme pour Client les informations de connexion
+ * sont lu soit directement à partir de l'env soit donnée en paramêtre
+ */
+const debug = require('debug')('SQL:log');
+const { Pool } = require('pg');
+
+const pool = new Pool();
+
+module.exports = {
+  // On expose le client original "au cas ou"
+  originalClient: pool,
+
+  // Une méthode pour "intercepter"
+  // les requêtes afin de pouvoir les afficher
+  // L'opérateur de "rest" permet de transformer
+  // ici X variables en param. en un tableau
+  async query(...params) {
+    debug(...params);
+
+    // L'opérateur ici fait l'effet inverse on transforme
+    // un tableau en une liste
+    // de variables / paramètre ce qui fait que la méthode query du client sera
+    // appelé exactement de la même façon que celle de notre module
+    return this.originalClient.query(...params);
+  },
+};
